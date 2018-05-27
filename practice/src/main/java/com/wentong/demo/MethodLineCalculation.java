91ba6b37f0fa03d0f136124af31016ee03cf0df6
zhouwentong <zhouwentong1993@gmail.com>
2018-05-28 00:13:42 +0800

    update

--- practice/src/main/java/com/wentong/demo/MethodLineCalculation.java
+++ practice/src/main/java/com/wentong/demo/MethodLineCalculation.java
@@ -11,9 +11,7 @@ import java.io.FileReader;
 public class MethodLineCalculation {
 
     private boolean startAnnotation = false;
-    private boolean endAnnotation = false;
     private boolean startMethod = false;
-    private boolean endMethod = false;
     private int lineNumber = 0;
 
     public static void main(String[] args) throws Exception{
