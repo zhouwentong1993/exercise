5649da6c9278c77d355d58fd79de8b77caa29b4b
zhouwentong <zhouwentong1993@gmail.com>
2018-07-24 00:11:26 +0800

    temp，为操作 redis 搞的

---
+++ redisdemo/src/main/java/Application.java
@@ -0,0 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zhouwentong on 2018/7/24.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
