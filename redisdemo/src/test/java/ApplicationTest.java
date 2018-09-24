98b506738ba451e3eda2950825b79fbcbb99a9f8
zhouwentong <zhouwentong1993@gmail.com>
2018-09-24 16:48:12 +0800

    增加 timeServer

--- redisdemo/src/test/java/ApplicationTest.java
+++ redisdemo/src/test/java/ApplicationTest.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
